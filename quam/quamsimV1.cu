a6d9d6d801469c7ab6dab81f0ec1c34c7f83c04f
Snehith Reyya <50079662+reysnell03@users.noreply.github.com>
2025-02-28 15:48:01 -0500

    Final quamsimV1.cu

--- quam/quamsimV1.cu
+++ quam/quamsimV1.cu
@@ -4,58 +4,80 @@
 #include <cuda_runtime.h>
 #include <iomanip>
 
-__global__ void quantum_gate_transform(const float *input, float *output, const float *gate, int size, int target) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    int bit_flip = 1 << target;
-    int paired_idx = idx ^ bit_flip;
+using namespace std;
 
-    if (idx < size && paired_idx < size) {
-        if ((idx / bit_flip) % 2 == 0) { 
-            output[idx] = gate[0] * input[idx] + gate[1] * input[paired_idx];
-            output[paired_idx] = gate[2] * input[idx] + gate[3] * input[paired_idx];
-        }
+__global__ void apply_quantum_gate(const float *input, float *output, const float *matrix, int size, int qubit) {
+    int in1 = blockIdx.x * blockDim.x + threadIdx.x;
+    int mask = 1 << qubit;
+    int in2 = in1 ^ mask;
+    bool is_lower_half = (in1 & mask) == 0;
+    float a, b, res1, res2;
+
+    if (in1 < size && in2 < size && is_lower_half) { 
+        a = input[in1];
+        b = input[in2];
+
+        res1 = fmaf(matrix[0], a, matrix[1] * b);  
+        res2 = fmaf(matrix[2], a, matrix[3] * b);
+
+        output[in1] = res1;
+        output[in2] = res2;
     }
 }
 
+
+
+
+
+
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <input_file>\n";
-        return EXIT_FAILURE;
-    }
 
-    std::ifstream input_file(argv[1]);
-    if (!input_file) {
-        std::cerr << "Error: Unable to open file " << argv[1] << "\n";
-        return EXIT_FAILURE;
-    }
-
+    ifstream input_file;
     float gate_matrix[4];
+    vector<float> state_vector;
+    float val;
+    int qubit_index, vector_length, threads_per_block, blocks_per_grid;
+    size_t state_bytes, gate_bytes;
+    float *host_input, *host_output, *host_gate;
+    float *device_input, *device_output, *device_gate;
+    cudaEvent_t start_time, stop_time;
+    float execution_duration;
+    cudaError_t error_status;
+
+    if (argc < 2) {
+        cerr << "Usage: " << argv[0] << " <input_file>\n";
+        return EXIT_FAILURE;
+    }
+
+    input_file.open(argv[1]);
+    if (!input_file) {
+        cerr << "Error: Unable to open file " << argv[1] << "\n";
+        return EXIT_FAILURE;
+    }
+
     for (int i = 0; i < 4; ++i) {
         input_file >> gate_matrix[i];
     }
 
-    std::vector<float> state_vector;
-    float val;
     while (input_file >> val) {
         state_vector.push_back(val);
     }
     input_file.close();
 
-    int qubit_index = static_cast<int>(state_vector.back());
+    qubit_index = static_cast<int>(state_vector.back());
     state_vector.pop_back();
     
-    int vector_length = state_vector.size();
-    size_t state_bytes = vector_length * sizeof(float);
-    size_t gate_bytes = 4 * sizeof(float);
+    vector_length = state_vector.size();
+    state_bytes = vector_length * sizeof(float);
+    gate_bytes = 4 * sizeof(float);
 
-    float *host_input = (float*)malloc(state_bytes);
-    float *host_output = (float*)malloc(state_bytes);
-    float *host_gate = (float*)malloc(gate_bytes);
+    host_input = (float*)malloc(state_bytes);
+    host_output = (float*)malloc(state_bytes);
+    host_gate = (float*)malloc(gate_bytes);
 
-    std::copy(state_vector.begin(), state_vector.end(), host_input);
-    std::copy(gate_matrix, gate_matrix + 4, host_gate);
+    copy(state_vector.begin(), state_vector.end(), host_input);
+    copy(gate_matrix, gate_matrix + 4, host_gate);
 
-    float *device_input, *device_output, *device_gate;
     cudaMalloc((void**)&device_input, state_bytes);
     cudaMalloc((void**)&device_output, state_bytes);
     cudaMalloc((void**)&device_gate, gate_bytes);
@@ -63,34 +85,32 @@ int main(int argc, char *argv[]) {
     cudaMemcpy(device_input, host_input, state_bytes, cudaMemcpyHostToDevice);
     cudaMemcpy(device_gate, host_gate, gate_bytes, cudaMemcpyHostToDevice);
 
-    cudaEvent_t start_time, stop_time;
     cudaEventCreate(&start_time);
     cudaEventCreate(&stop_time);
 
-    int threads_per_block = 256;
-    int blocks_per_grid = (vector_length + threads_per_block - 1) / threads_per_block;
+    threads_per_block = 256;
+    blocks_per_grid = (vector_length + threads_per_block - 1) / threads_per_block;
 
     cudaEventRecord(start_time, 0);
-    quantum_gate_transform<<<blocks_per_grid, threads_per_block>>>(device_input, device_output, device_gate, vector_length, qubit_index);
+    apply_quantum_gate<<<blocks_per_grid, threads_per_block>>>(device_input, device_output, device_gate, vector_length, qubit_index);
     cudaEventRecord(stop_time, 0);
     cudaEventSynchronize(stop_time);
 
-    float execution_duration;
     cudaEventElapsedTime(&execution_duration, start_time, stop_time);
 
-    cudaError_t error_status = cudaGetLastError();
+    error_status = cudaGetLastError();
     if (error_status != cudaSuccess) {
-        std::cerr << "CUDA Error: " << cudaGetErrorString(error_status) << "\n";
+        cerr << "CUDA Error: " << cudaGetErrorString(error_status) << "\n";
         return EXIT_FAILURE;
     }
 
     cudaMemcpy(host_output, device_output, state_bytes, cudaMemcpyDeviceToHost);
 
     for (int i = 0; i < vector_length; ++i) {
-        std::cout << std::fixed << std::setprecision(3) << host_output[i] << "\n";
+        cout << fixed << setprecision(3) << host_output[i] << "\n";
     }
 
-    std::cout << "Execution Time: " << execution_duration << " ms\n";
+  //  cout << "Execution Time: " << execution_duration << " ms\n";
 
     cudaFree(device_input);
     cudaFree(device_output);
